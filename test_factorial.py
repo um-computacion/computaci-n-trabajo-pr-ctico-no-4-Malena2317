c2e86b12954323fdded27e65ed8ab826744b54ce
Malena2317 <me.navarro@alumno.um.edu.ar>
2025-05-01 11:41:12 -0300

    subiendo los test de la funcion factorial seguiendo la metodologia TDD

---
+++ test_factorial.py
@@ -0,0 +1,32 @@
+import unittest
+
+
+def factorial(n): # todavia no se implementa la funcion 
+    pass 
+
+class TestFactorial(unittest.TestCase):
+    
+    def test_factorial_0(self):
+        # El factorial de 0 debe ser 1
+        self.assertEqual(factorial(0), 1)
+    
+    def test_factorial_enteros(self):
+        # El factorial de 1 , 10 y 5 debe ser 1,3628800 y 120
+        self.assertEqual(factorial(1), 1)
+        self.assertEqual(factorial(10), 3628800)
+        self.assertEqual(factorial(5), 120)
+    
+    def test_factorial_negativo(self):
+        # Los números negativos deberían dar error
+        with self.assertRaises(ValueError):
+            factorial(-1)
+    
+    def test_factorial_decimal(self):
+        # Los números decimales deberían dar error
+        with self.assertRaises(ValueError):
+            factorial(1.5)
+
+
+# Para poder ejecutar los tests
+if __name__ == "__main__":
+    unittest.main()
